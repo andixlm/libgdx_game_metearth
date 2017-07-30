cc2e7372d39a4d6e620695825d394800494ed2e6
andixlm <andixev@gmail.com>
2017-07-30 18:35:04 +0300

    GameWorld: Initial implementation

--- core/src/com/ankreez/metearth/Game/GameWorld.java
+++ core/src/com/ankreez/metearth/Game/GameWorld.java
@@ -2,12 +2,24 @@ package com.ankreez.metearth.Game;
 
 public class GameWorld {
 
-    public GameWorld() {
+    private float mWorldWidth;
+    private float mWorldHeight;
 
+    public GameWorld(float worldWidth, float worldHeight) {
+        mWorldWidth = worldWidth;
+        mWorldHeight = worldHeight;
     }
 
     public void update(float delta) {
 
+    }
+
+    public float getGameWidth() {
+        return mWorldWidth;
+    }
+
+    public float getGameHeight() {
+        return mWorldHeight;
     }
 
 }

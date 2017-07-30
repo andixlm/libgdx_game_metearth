86b4c7176690f11e94c9b1018ad904c5f3f94bea
andixlm <andixev@gmail.com>
2017-07-30 17:01:09 +0300

    GameScreen: Implement game rendering

--- core/src/com/ankreez/metearth/Screens/GameScreen.java
+++ core/src/com/ankreez/metearth/Screens/GameScreen.java
@@ -1,11 +1,25 @@
 package com.ankreez.metearth.Screens;
 
+import com.ankreez.metearth.Game.GameRenderer;
+import com.ankreez.metearth.Game.GameWorld;
 import com.badlogic.gdx.Game;
 
 public class GameScreen extends MetEarthScreen {
 
+    private GameWorld mGameWorld;
+    private GameRenderer mGameRenderer;
+
     public GameScreen(Game game) {
         super(game);
+
+        mGameWorld = new GameWorld();
+        mGameRenderer = new GameRenderer();
+    }
+
+    @Override
+    public void render(float delta) {
+        mGameWorld.update(delta);
+        mGameRenderer.render(delta);
     }
 
 }

fb431728931e472a1c0812e3e58096873eb57138
andixlm <andixev@gmail.com>
2017-07-30 19:05:48 +0300

    GameWorld: Add Earth

--- core/src/com/ankreez/metearth/Game/GameWorld.java
+++ core/src/com/ankreez/metearth/Game/GameWorld.java
@@ -1,13 +1,22 @@
 package com.ankreez.metearth.Game;
 
+import com.ankreez.metearth.Objects.Earth;
+
 public class GameWorld {
 
     private float mWorldWidth;
     private float mWorldHeight;
 
+    private Earth mEarth;
+
     public GameWorld(float worldWidth, float worldHeight) {
         mWorldWidth = worldWidth;
         mWorldHeight = worldHeight;
+
+        float earthWidth = mWorldWidth / 4.0f;
+        float earthHeight = earthWidth;
+        mEarth = new Earth((mWorldWidth - earthWidth) / 2.0f, (mWorldHeight - earthHeight) / 2.0f,
+                earthWidth, earthHeight);
     }
 
     public void update(float delta) {
@@ -20,6 +29,10 @@ public class GameWorld {
 
     public float getGameHeight() {
         return mWorldHeight;
+    }
+
+    public Earth getEarth() {
+        return mEarth;
     }
 
 }

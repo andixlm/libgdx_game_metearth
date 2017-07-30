169ddec5802c32fe644adbaecdf02982f0f0f7b0
andixlm <andixev@gmail.com>
2017-07-30 18:40:04 +0300

    GameScreen: Calculate world size

--- core/src/com/ankreez/metearth/Screens/GameScreen.java
+++ core/src/com/ankreez/metearth/Screens/GameScreen.java
@@ -3,8 +3,13 @@ package com.ankreez.metearth.Screens;
 import com.ankreez.metearth.Game.GameRenderer;
 import com.ankreez.metearth.Game.GameWorld;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class GameScreen extends MetEarthScreen {
+
+    private static final float WORLD_WIDTH = 128.0f;
+    private static final float WORLD_HEIGHT = (float) Gdx.graphics.getHeight() /
+            ((float) Gdx.graphics.getWidth() / WORLD_WIDTH);
 
     private GameWorld mGameWorld;
     private GameRenderer mGameRenderer;

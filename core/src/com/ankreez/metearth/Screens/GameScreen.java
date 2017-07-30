1fb1b2e4207d18876f8b6e8b6436856e1dbd79d1
andixlm <andixev@gmail.com>
2017-07-30 14:30:33 +0300

    screen: Implement game screen

---
+++ core/src/com/ankreez/metearth/Screens/GameScreen.java
@@ -0,0 +1,11 @@
+package com.ankreez.metearth.Screens;
+
+import com.badlogic.gdx.Game;
+
+public class GameScreen extends MetEarthScreen {
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+}

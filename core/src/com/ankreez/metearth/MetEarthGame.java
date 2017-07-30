361043aace5a3bfd8a69fa5dd4a0ac08644b90b2
andixlm <andixev@gmail.com>
2017-07-30 14:31:32 +0300

    game: Set game screen on start

--- core/src/com/ankreez/metearth/MetEarthGame.java
+++ core/src/com/ankreez/metearth/MetEarthGame.java
@@ -1,12 +1,13 @@
 package com.ankreez.metearth;
 
+import com.ankreez.metearth.Screens.GameScreen;
 import com.badlogic.gdx.Game;
 
 public class MetEarthGame extends Game {
 
     @Override
     public void create() {
-
+        setScreen(new GameScreen(this));
     }
 
 }

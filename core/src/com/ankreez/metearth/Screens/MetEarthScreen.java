fcd02b2ec8ac8f0873bf3f2f695b56719dcc007b
andixlm <andixev@gmail.com>
2017-07-30 14:27:08 +0300

    screen: Implement abstract game screen

---
+++ core/src/com/ankreez/metearth/Screens/MetEarthScreen.java
@@ -0,0 +1,48 @@
+package com.ankreez.metearth.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public abstract class MetEarthScreen implements Screen {
+
+    protected Game mGame;
+
+    public MetEarthScreen(Game game) {
+        mGame = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

b7b3ef8b6a9d67f79334b5fc5fa41df7bb772a1b
andixlm <andixev@gmail.com>
2017-07-31 22:24:18 +0300

    objects: Implement Portal

---
+++ core/src/com/ankreez/metearth/Objects/Portal.java
@@ -0,0 +1,38 @@
+package com.ankreez.metearth.Objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Portal extends GameObject {
+
+    private Portal mOutPortal;
+
+    public Portal() {
+        super();
+    }
+
+    public Portal(float x, float y, float radius) {
+        super(x, y, radius, radius);
+    }
+
+    public Portal(Vector2 position, float radius) {
+        super(position, radius, radius);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    public void onClick(float x, float y) {
+        setPosition(x, y);
+    }
+
+    public Portal getOutPortal() {
+        return mOutPortal;
+    }
+
+    public void setOutPortal(Portal outPortal) {
+        mOutPortal = outPortal;
+    }
+
+}

e64638a40e782ee30c7ffdcad7c5fbac97aba3a4
andixlm <andixev@gmail.com>
2017-08-01 23:09:00 +0300

    GameScreen: Use ASPECT_RATION constant

--- core/src/com/ankreez/metearth/Screens/GameScreen.java
+++ core/src/com/ankreez/metearth/Screens/GameScreen.java
@@ -7,9 +7,10 @@ import com.badlogic.gdx.Gdx;
 
 public class GameScreen extends MetEarthScreen {
 
+    private static final float ASPECT_RATIO = Gdx.graphics.getHeight() / Gdx.graphics.getWidth();
+
     private static final float WORLD_WIDTH = 128.0f;
-    private static final float WORLD_HEIGHT = (float) Gdx.graphics.getHeight() /
-            ((float) Gdx.graphics.getWidth() / WORLD_WIDTH);
+    private static final float WORLD_HEIGHT = WORLD_WIDTH * ASPECT_RATIO;
 
     private GameWorld mGameWorld;
     private GameRenderer mGameRenderer;

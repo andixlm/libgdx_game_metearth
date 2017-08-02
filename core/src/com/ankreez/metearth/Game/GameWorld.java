f3a0f17703e9f9190791c57768c36eb61e76927f
andixlm <andixev@gmail.com>
2017-08-02 19:17:34 +0300

    GameWorld: Add Wormhole

--- core/src/com/ankreez/metearth/Game/GameWorld.java
+++ core/src/com/ankreez/metearth/Game/GameWorld.java
@@ -1,6 +1,8 @@
 package com.ankreez.metearth.Game;
 
 import com.ankreez.metearth.Objects.Earth;
+import com.ankreez.metearth.Objects.Portal;
+import com.ankreez.metearth.Objects.Wormhole;
 
 public class GameWorld {
 
@@ -8,6 +10,7 @@ public class GameWorld {
     private float mWorldHeight;
 
     private Earth mEarth;
+    private Wormhole mWormhole;
 
     public GameWorld(float worldWidth, float worldHeight) {
         mWorldWidth = worldWidth;
@@ -17,6 +20,13 @@ public class GameWorld {
         float earthHeight = earthWidth;
         mEarth = new Earth((mWorldWidth - earthWidth) / 2.0f, (mWorldHeight - earthHeight) / 2.0f,
                 earthWidth, earthHeight);
+
+        float portalRadius = earthWidth / 2.0f;
+        float portalPositionX = (mWorldWidth - portalRadius) / 2.0f;
+        float portalPositionY = (mWorldHeight - 2.0f * portalRadius) / 4.0f;
+        mWormhole = new Wormhole(new Portal(portalPositionX, portalPositionY, portalRadius),
+                new Portal(portalPositionX, mWorldHeight - portalPositionY, portalRadius));
+
     }
 
     public void update(float delta) {

0290bcb1bfd6ee4f900ef7132edf74da21333cb8
andixlm <andixev@gmail.com>
2017-08-03 19:56:42 +0300

    GameWorld: Add meteorites

--- core/src/com/ankreez/metearth/Game/GameWorld.java
+++ core/src/com/ankreez/metearth/Game/GameWorld.java
@@ -1,16 +1,21 @@
 package com.ankreez.metearth.Game;
 
 import com.ankreez.metearth.Objects.Earth;
+import com.ankreez.metearth.Objects.Meteorite;
 import com.ankreez.metearth.Objects.Portal;
 import com.ankreez.metearth.Objects.Wormhole;
+import com.badlogic.gdx.utils.Array;
 
 public class GameWorld {
+
+    private static final byte METEORITES_COUNT = 4;
 
     private float mWorldWidth;
     private float mWorldHeight;
 
     private Earth mEarth;
     private Wormhole mWormhole;
+    private Array<Meteorite> mMeteorites;
 
     public GameWorld(float worldWidth, float worldHeight) {
         mWorldWidth = worldWidth;
@@ -28,6 +33,11 @@ public class GameWorld {
         mWormhole = new Wormhole(new Portal(portalPositionX, portalPositionY, portalRadius),
                 new Portal(portalPositionX, mWorldHeight - portalPositionY, portalRadius));
 
+        float meteoriteRadius = portalRadius;
+        mMeteorites = new Array<Meteorite>(false, METEORITES_COUNT);
+        for (int count = 0; count < METEORITES_COUNT; ++count) {
+            mMeteorites.add(new Meteorite(meteoriteRadius, mWorldWidth, mWorldHeight));
+        }
     }
 
     public void update(float delta) {

44d4191d3c32ea2247326b1d47b3e0dbc2c73b41
andixlm <andixev@gmail.com>
2017-08-04 22:48:59 +0300

    GameRenderer: Render objects

--- core/src/com/ankreez/metearth/Game/GameRenderer.java
+++ core/src/com/ankreez/metearth/Game/GameRenderer.java
@@ -1,21 +1,83 @@
 package com.ankreez.metearth.Game;
 
+import com.ankreez.metearth.Helpers.AssetHelper;
+import com.ankreez.metearth.Objects.Earth;
+import com.ankreez.metearth.Objects.Meteorite;
+import com.ankreez.metearth.Objects.Portal;
+import com.ankreez.metearth.Objects.Wormhole;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 public class GameRenderer {
 
     private GameWorld mGameWorld;
+
+    private Earth mEarth;
+    private Wormhole mWormhole;
+    private Array<Meteorite> mMeteorites;
+
+    private TextureRegion mEarthTexture;
+    private TextureRegion mPortalTexture;
+    private TextureRegion mMeteoriteTexture;
+
     private OrthographicCamera mCamera;
+    
+    private SpriteBatch mSpriteRenderer;
 
     public GameRenderer(GameWorld gameWorld) {
         mGameWorld = gameWorld;
 
+        initObjects();
+        initAssets();
+
         mCamera = new OrthographicCamera();
         mCamera.setToOrtho(false, mGameWorld.getWorldWidth(), mGameWorld.getWorldHeight());
+
+        mSpriteRenderer = new SpriteBatch();
+        mSpriteRenderer.setProjectionMatrix(mCamera.combined);
     }
 
     public void render(float delta) {
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        mSpriteRenderer.begin();
+
+        mSpriteRenderer.draw(mEarthTexture,
+                mEarth.getX(), mEarth.getY(),
+                mEarth.getWidth(), mEarth.getHeight());
+
+        for (Portal portal : mWormhole) {
+            mSpriteRenderer.draw(mPortalTexture,
+                    portal.getX(), portal.getY(),
+                    portal.getWidth(), portal.getHeight());
+        }
+
+        for (Meteorite meteorite : mMeteorites) {
+            mSpriteRenderer.draw(mMeteoriteTexture,
+                    meteorite.getX(), meteorite.getY(),
+                    meteorite.getWidth() / 2.0f, meteorite.getHeight() / 2.0f,
+                    meteorite.getWidth(), meteorite.getHeight(),
+                    1.0f, 1.0f, meteorite.getRotation());
+        }
+
+        mSpriteRenderer.end();
+    }
+
+    private void initObjects() {
+        mEarth = mGameWorld.getEarth();
+        mWormhole = mGameWorld.getWormhole();
+        mMeteorites = mGameWorld.getMeteorites();
+    }
+
+    private void initAssets() {
+        mEarthTexture = AssetHelper.sEarthTexture;
+        mPortalTexture = AssetHelper.sPortalTexture;
+        mMeteoriteTexture = AssetHelper.sMeteoriteTexture;
     }
 
     public GameWorld getGameWorld() {

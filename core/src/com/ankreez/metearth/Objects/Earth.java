0df5e01a61756c8116576bf2b2260536f13c08a4
andixlm <andixev@gmail.com>
2017-08-06 12:00:45 +0300

    Earth: Add bounding circle

--- core/src/com/ankreez/metearth/Objects/Earth.java
+++ core/src/com/ankreez/metearth/Objects/Earth.java
@@ -1,8 +1,11 @@
 package com.ankreez.metearth.Objects;
 
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Earth extends SimpleObject {
+
+    private Circle mBounds;
 
     public Earth() {
         super();
@@ -18,6 +21,8 @@ public class Earth extends SimpleObject {
 
     public Earth(float x, float y, float width, float height) {
         super(x, y, width, height);
+
+        mBounds = new Circle(x + width / 2.0f, y + height / 2.0f, width / 2.0f);
     }
 
     public Earth(Vector2 position, float width, float height) {
@@ -27,6 +32,10 @@ public class Earth extends SimpleObject {
     @Override
     public void update(float delta) {
 
+    }
+
+    public Circle getBounds() {
+        return mBounds;
     }
 
 }

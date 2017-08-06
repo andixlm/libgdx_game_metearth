b6111d950fc16b8c1f64d5471603347e3fad3905
andixlm <andixev@gmail.com>
2017-08-06 14:05:29 +0300

    Portal: Add bounding rectangle

--- core/src/com/ankreez/metearth/Objects/Portal.java
+++ core/src/com/ankreez/metearth/Objects/Portal.java
@@ -1,13 +1,22 @@
 package com.ankreez.metearth.Objects;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Portal extends SimpleObject {
+
+    private static final float BOUNDS_POS_MULT = 0.75f;
+    private static final float BOUNDS_SIZE_MULT = 0.5f;
+
+    private Rectangle mBounds;
 
     private Portal mOutPortal;
 
     public Portal(float x, float y, float radius) {
         super(x, y, 2.0f * radius, 2.0f * radius);
+
+        mBounds = new Rectangle(x + BOUNDS_POS_MULT * radius, y + BOUNDS_POS_MULT * radius,
+                radius * BOUNDS_SIZE_MULT, radius * BOUNDS_SIZE_MULT);
     }
 
     public Portal(Vector2 position, float radius) {
@@ -21,6 +30,9 @@ public class Portal extends SimpleObject {
 
     public void onClick(float x, float y) {
         setPosition(x, y);
+
+        mBounds.setPosition(getX() + BOUNDS_POS_MULT * getRadius(),
+                getY() + BOUNDS_POS_MULT * getRadius());
     }
 
     public float getRadius() {
@@ -30,6 +42,10 @@ public class Portal extends SimpleObject {
     public void setRadius(float radius) {
         setWidth(2.0f * radius);
         setHeight(2.0f * radius);
+    }
+
+    public Rectangle getBounds() {
+        return mBounds;
     }
 
     public Portal getOutPortal() {

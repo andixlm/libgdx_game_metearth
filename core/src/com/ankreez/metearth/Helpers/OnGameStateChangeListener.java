6743b8375c580222a59485d621bef579c6b8226c
andixlm <andixev@gmail.com>
2017-08-12 23:51:31 +0300

    helpers: Create OnGameStateChangeListener interface

---
+++ core/src/com/ankreez/metearth/Helpers/OnGameStateChangeListener.java
@@ -0,0 +1,9 @@
+package com.ankreez.metearth.Helpers;
+
+import com.ankreez.metearth.Game.GameWorld;
+
+public interface OnGameStateChangeListener {
+
+    public void onGameStateChange(GameWorld.GameState gameState);
+
+}

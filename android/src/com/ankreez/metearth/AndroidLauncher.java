a99caa0205c03d61f922d5f3fe2637e89cdd74e8
andixlm <andixev@gmail.com>
2017-08-15 23:03:46 +0300

    AndroidLauncher: Store game view

--- android/src/com/ankreez/metearth/AndroidLauncher.java
+++ android/src/com/ankreez/metearth/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.ankreez.metearth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -8,15 +10,23 @@ import com.ankreez.metearth.MetEarthGame;
 
 public class AndroidLauncher extends AndroidApplication {
 
+    View mGameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useAccelerometer = false;
         config.useCompass = false;
 
-        initialize(new MetEarthGame(), config);
+        mGameView = initializeForView(new MetEarthGame(), config);
+
+        setContentView(mGameView, createLayoutParams());
     }
 
 }

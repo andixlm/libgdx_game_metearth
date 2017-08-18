6dd7706a2cfaa3ebf1fcea8955e2abcb28b7082b
andixlm <andixev@gmail.com>
2017-08-18 21:00:48 +0300

    AndroidLauncher: Add ad view to the top of screen

--- android/src/com/ankreez/metearth/AndroidLauncher.java
+++ android/src/com/ankreez/metearth/AndroidLauncher.java
@@ -2,15 +2,25 @@ package com.ankreez.metearth;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.ankreez.metearth.MetEarthGame;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
 
+    // Test ad is used by default.
+    private static final String BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
+
+    LinearLayout mGameLayout;
+
     View mGameView;
+    AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +34,23 @@ public class AndroidLauncher extends AndroidApplication {
         config.useAccelerometer = false;
         config.useCompass = false;
 
-        mGameView = initializeForView(new MetEarthGame(), config);
+        mGameLayout = new LinearLayout(this);
+        mGameLayout.setOrientation(LinearLayout.VERTICAL);
 
-        setContentView(mGameView, createLayoutParams());
+        mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.BANNER);
+        mAdView.setAdUnitId(BANNER_AD_UNIT_ID);
+        mGameLayout.addView(mAdView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        mGameView = initializeForView(new MetEarthGame(), config);
+        mGameLayout.addView(mGameView, new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f
+        ));
+
+        setContentView(mGameLayout, createLayoutParams());
+
+        mAdView.loadAd(new AdRequest.Builder().build());
     }
 
 }

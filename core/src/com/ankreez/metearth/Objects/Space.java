8e3b39c07c667ff812a23e57f46f1109c3755022
andixlm <andixev@gmail.com>
2017-08-19 13:07:49 +0300

    objects: Implement Space

---
+++ core/src/com/ankreez/metearth/Objects/Space.java
@@ -0,0 +1,67 @@
+package com.ankreez.metearth.Objects;
+
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Space implements Iterable<Star> {
+
+    private static final int STARS_AMOUNT = 13;
+
+    private static final float STAR_RADIUS = 2.5f;
+
+    private static final Random sRandom  = new Random(TimeUtils.millis());
+
+    private Star[] mStars;
+
+    public Space(float worldWidth, float worldHeight) {
+        mStars = new Star[STARS_AMOUNT];
+
+        initStars();
+        resetStars(worldWidth, worldHeight);
+    }
+
+    private void initStars() {
+        for (int idx = 0; idx < STARS_AMOUNT; ++idx) {
+            mStars[idx] = new Star(0.0f, 0.0f, STAR_RADIUS, STAR_RADIUS);
+        }
+    }
+
+    private void resetStars(float worldWidth, float worldHeight) {
+        for (int idx = 0; idx < STARS_AMOUNT; ++idx) {
+            mStars[idx].setPosition(sRandom.nextFloat() * worldWidth,
+                    sRandom.nextFloat() * worldHeight);
+        }
+    }
+
+    @Override
+    public Iterator<Star> iterator() {
+        return new SpaceIterator();
+    }
+
+    private class SpaceIterator implements Iterator<Star> {
+
+        private int mIndex;
+
+        private SpaceIterator() {
+            mIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return mIndex < STARS_AMOUNT;
+        }
+
+        @Override
+        public Star next() {
+            return mStars[mIndex++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+}
